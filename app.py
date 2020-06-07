b35e1507ec2f959920a45fe261b6d38b61c6b19f
Renuka Jagdale <49937707+RenukaJagdale@users.noreply.github.com>
2020-06-08 02:59:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import pickle
 
 # load the model from disk
 filename = 'nlp_model.pkl'
-clf = pickle.load(open('nlp_model_xgb.pkl','rb'))
+clf = pickle.load(open('nlp_model_new.pkl','rb'))
 cv=pickle.load(open('tweet_tranform_new.pkl','rb'))
 app = Flask(__name__)
 
